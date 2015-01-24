f4b4d00ee2f6e102330bf97a44db49836c28a5af
jackmacattack <jackmacattack92@gmail.com>
2015-01-24 17:41:51 -0500

    Start of controllers

---
+++ RoboSurvive/Assets/Scripts/Controller.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controller : MonoBehaviour {
+
+    private Expansion expand;
+    private Upkeep upkeep;
+
+	// Use this for initialization
+    void Start()
+    {
+        expand = this.GetComponent<Expansion>();
+        upkeep = this.GetComponent<Upkeep>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.anyKeyDown) {
+            Run();
+        }
+	}
+
+    private void RandomEvents()
+    {
+
+    }
+
+    public void Run()
+    {
+        expand.Run();
+        upkeep.Run();
+    }
+}
