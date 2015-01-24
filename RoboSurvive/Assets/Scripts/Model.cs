5786c3742baa94fd875411f47faccb013447350f
SebastianJay <sebastianjay2@gmail.com>
2015-01-24 00:02:25 -0500

    Test Labels
    meh

---
+++ RoboSurvive/Assets/Scripts/Model.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/**
+ * for lack of a better name
+ * 
+ * Information about the game state here
+ */
+public class Model : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
