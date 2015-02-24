67af3f03c33c9db5164e77b60275c014d2f472b9
jackmacattack <jackmacattack92@gmail.com>
2015-02-23 21:38:36 -0500

    Created framework for simulation

---
+++ RoboSurvive/Assets/Scripts/Simulation/TestRunSimulation.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Tests the simulation. Will step the simulation on click.
+public class TestRunSimulation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        // Runs the simulation on mouse click.
+        if (Input.GetMouseButtonDown(0))
+        {
+            this.GetComponent<RunSimulation>().Run();
+        }
+	}
+}
