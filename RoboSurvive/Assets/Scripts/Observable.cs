bedf696db98c902c4444679beee7ef086b53ddbc
Dylan Hellems <dylanhellems@gmail.com>
2015-02-23 22:22:31 -0500

    Model-Observer scripts plus example use

---
+++ RoboSurvive/Assets/Scripts/Observable.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Observable : MonoBehaviour {
+
+	// Collection of observers attached to this observable
+	public List<Observer> observers;
+
+	// Initializes
+	public void Start() {
+
+	}
+
+	// Adds o to observers
+	public void Attach(Observer o) {
+		observers.Add(o);
+	}
+
+	// Removes o from observers
+	public void Detach(Observer o) {
+		observers.Remove(o);
+	}
+
+	// Calls notify on each observer in observers
+	public void NotifyAll(string field, int val) {
+		foreach (Observer o in observers) {
+			o.Notify(field, val);
+		}
+	}
+}
