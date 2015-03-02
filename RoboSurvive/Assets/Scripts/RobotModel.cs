df643eb8544913e3697136646878147ab9c57257
Dylan Hellems <dylanhellems@gmail.com>
2015-03-02 18:19:42 -0500

    Formatting

--- RoboSurvive/Assets/Scripts/RobotModel.cs
+++ RoboSurvive/Assets/Scripts/RobotModel.cs
@@ -11,7 +11,7 @@ public class RobotModel : Observable {
 	// Initializes
 	new public void Start() {
 		base.Start();
-		this.markOne = 500;
+		this.markOne = 0;
 		this.markTwo = 0;
 		this.markThree = 0;
 
@@ -20,36 +20,36 @@ public class RobotModel : Observable {
 	// Returns copy of this model
 	public RobotModel Clone() {
 		RobotModel m = new RobotModel();
-		m.SetOne(markOne);
-		m.SetTwo(markTwo);
-		m.SetThree(markThree);
+		m.SetMarkOne(markOne);
+		m.SetMarkTwo(markTwo);
+		m.SetMarkThree(markThree);
 		return m;
 	}
 	
 	// Getters and Setters
 	
-	public int GetOne() {
+	public int GetMarkOne() {
 		return this.markOne;
 	}
-	public void SetOne(float value) {
+	public void SetMarkOne(float value) {
 		markOne = (int)value;
-		NotifyAll("mark one", (int)value);
+		NotifyAll("mk1", (int)value);
 	}
 	
-	public int GetTwo() {
+	public int GetMarkTwo() {
 		return this.markTwo;
 	}
-	public void SetTwo(int value) {
-		markTwo = value;
-		NotifyAll("mark two", value);
+	public void SetMarkTwo(float value) {
+		markTwo = (int)value;
+		NotifyAll("mk2", (int)value);
 	}
 	
-	public int GetThree() {
+	public int GetMarkThree() {
 		return this.markThree;
 	}
-	public void SetThree(int value) {
-		markThree = value;
-		NotifyAll("mark three", value);
+	public void SetMarkThree(float value) {
+		markThree = (int)value;
+		NotifyAll("mk3", (int)value);
 	}
 
 }
