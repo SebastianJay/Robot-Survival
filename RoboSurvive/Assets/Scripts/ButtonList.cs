3120242292615a586d15ff32acc00699b334fb9a
Dylan Hellems <dylanhellems@gmail.com>
2015-04-13 17:09:32 -0400

    Maybe buttons

---
+++ RoboSurvive/Assets/Scripts/ButtonList.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ButtonList : MonoBehaviour {
+
+	public DevelopmentsModel devs;
+	public Transform contentPanel;
+
+	// Use this for initialization
+	void Start () {
+		this.PopulateList ();
+	}
+	
+	// Populates list of buttons
+	void PopulateList () {
+		foreach (string dev in devs.GetUndeveloped()) {
+			GameObject button = new GameObject();
+			button.AddComponent<RectTransform>();
+			button.AddComponent<Button>();
+			button.AddComponent<Text>();
+			button.GetComponent<Text>().text = dev;
+			button.transform.SetParent(contentPanel);		
+		}
+	}
+}
