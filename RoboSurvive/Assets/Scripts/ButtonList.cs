efc0a0555a9139f405b769b98edc7ae9f4dddc23
Dylan Hellems <dylanhellems@gmail.com>
2015-04-27 18:21:35 -0400

    Masked

--- RoboSurvive/Assets/Scripts/ButtonList.cs
+++ RoboSurvive/Assets/Scripts/ButtonList.cs
@@ -6,6 +6,7 @@ public class ButtonList : MonoBehaviour {
 
 	public DevelopmentsModel devs;
 	public Transform contentPanel;
+	public Transform posPanel;
 	public Transform theButton;
 
 	// Use this for initialization
@@ -24,7 +25,7 @@ public class ButtonList : MonoBehaviour {
 					text.text = dev;
 				}
 			}
-			Vector3 pos = contentPanel.position;
+			Vector3 pos = posPanel.position;
 			button.position = pos + new Vector3(-150, -50 - 50 * devs.GetUndeveloped().IndexOf(dev), 1);
 		}
 	}
