5bd3d2b8b3d8632a072da684039be39c782f4f52
jackmacattack <jackmacattack92@gmail.com>
2015-04-29 03:28:14 -0400

    Added metastuff

---
+++ RoboSurvive/Assets/Scripts/Simulation/Meta.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Meta : SimScript {
+
+	public int oilCost, metalCost, ComponentsCost;
+    public SoundMixer mixer;
+    public StorySelector sel;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public override void RunSimulation(Transform preTurn, Transform afterTurn, Transform result) {
+        var meta = result.GetComponent<MetaModel>();
+        meta.SetYear(meta.GetYear() + 1);
+
+        if (meta.year >= 2070)
+        {
+            mixer.PlayStage5();
+            sel.AddAct3 = true;
+        }
+        if (meta.year >= 2060)
+        {
+            mixer.PlayStage4();
+        }
+        if (meta.year >= 2055)
+        {
+            mixer.PlayStage3();
+            sel.AddAct2 = true;
+            sel.set = true;
+        }
+        if (meta.year >= 2050)
+        {
+            mixer.PlayStage2();
+        }
+        else 
+        {
+            mixer.PlayStage1();
+        }
+
+        if(meta.storyPath > 0 && !sel.set) {
+            sel.Sulphate = true;
+        }
+	}
+}
