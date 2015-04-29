1efbda9ceec315e78e6de08bb00b328e1959db39
jackmacattack <jackmacattack92@gmail.com>
2015-04-29 01:03:08 -0400

    Added Place

---
+++ RoboSurvive/Assets/Scripts/Simulation/Place.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Place : MonoBehaviour {
+    public string name;
+    public string desc;
+    public float difficulty, rarity;
+    public bool story, developement, resource, robots; 
+}
