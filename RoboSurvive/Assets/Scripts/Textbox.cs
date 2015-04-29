8002ca58f3c2a32e84b6da888eae49766e40f30a
stevenstetzler <believe.me.im.steve@gmail.com>
2015-04-29 04:13:21 -0400

    Tutorial

--- RoboSurvive/Assets/Scripts/Textbox.cs
+++ RoboSurvive/Assets/Scripts/Textbox.cs
@@ -3,141 +3,271 @@ using System.Collections;
 
 public class Textbox : MonoBehaviour {
 	
-	private Rect windowRect = new Rect(20, 200, 200, 200);
+	public NavBarManager manager;
 
-	private string factoryString = "This is the factory tab. Change robots between Mk I, Mk II, and Mk III";
-	private string tasksString = "In this tab, you assign robots to various tasks necessary for survival and expansion";
-	private string developmentsString = "In this tab, you will manage the development of your various systems";
-	private string resourceString = "These are your resources: Oil does this. Components do this. Metal does this";
-	private string robotString = "There are three types of robots. Mk I robots are good at this. Mk II are good at this." +
-		"Mk III are good at this.";
-	private string executeString = "Click this button to end the simulation";
-	public string playString = "You're all set! Click play to start the game!";
+	//window0
+	//Skip tutorial button
+	public Rect startUpRect;
+	public string startUpString = "This tutorial will guide you through the functions of your systems.";
+
+	//window1
+	public Rect robotRect;
+	public string robotString = "This is the number of robots currently functioning. Robots are the tools by which " +
+		"you will perform any and all tasks. Though you start with a relatively low number of Mk I's, you will be " +
+		"able to degrade these into a higher quantity of lower-tier robots. Be warned that once lost, you will never " +
+		"be able to regain your Mk I's. ";
+
+	public Rect robotBRect;
+	public string robotBString = "There are three classes of robots. " +
+		"Top-Tier: Mk I - high maintenance, high efficiency; able to perform all tasks " +
+		"Mid-Tier: Mk II - medium maintenance, medium efficiency; high level tasks locked " +
+		"Bottom-Tier: Mk III - low maintenance, low efficiency; only able to perform base functions ";
+
+	//window2
+	public Rect materialsRect;
+	public string materialsString = "These are the materials which you will manage. Components are used for developing the " +
+		"systems your currently have. " +
+		"Metal is used for the conversion process as well as building developments. " +
+		"Oil is used every turn to replenish your energy. If you run out of oil, you will be forced to terminate.";
+
+	//window3
+	public Rect tabsRect;
+	public string tabsString = "The tabs above will allow you to switch between the various screens in order to plan " +
+		"our your turn.";
+
+	//window4
+	//Go into the tab then do window
+	public Rect factoryRect;
+	public string factoryString = "This is the factory screen. It will allow you to degrade upper-tier robots to " +
+		"lower-tier robots.";
+
+	//window5
+	//Go to developments
+	public Rect developmentsRect;
+	public string developmentsString = "This is the developments screen. You will manage the development of your various " +
+		"systems.";
+
+	//window6
+	public Rect messagesRect;
+	public string messagesString = "This is the messages screen. It will alert you to new tasks and developments, as well " +
+		"as keep a record of previous turns.";
+
+	//window7
+	public Rect tasksRect;
+	public string tasksString = "This is the tasks screen. You assign robots to various tasks necessary for " +
+		"survival and expansion.";
+
+	//window8
+	//Go home
+	public Rect energyRect;
+	public string energyString = "This is the energy which is allotted to you to activate robots. It will replenish " +
+		"each turn at the expense of oil. It, like many other things, can be upgraded in the developments screen.";
+
+	//window9
+	public Rect executeRect;
+	public string executeString = "This will end your turn and execute all planned functions.";
+
+	//window10
+	//Button - exit tutorial
+	public Rect playRect;
+	public string playString = "Tutorial complete.";
 
 	public string currentMenu = "";
 
-	public Texture background;
-	public GUIStyle myStyle;
-	public GUIContent title;
-	
 	void Start () {
-		currentMenu = "window1";
+		manager.onClickHome ();
+		currentMenu = "window0";
 	}
 	void NavigateTo(string nextmenu) {
 		currentMenu = nextmenu;
 	}
 	void OnGUI() {
-		Color colGui = GUI.color;
-		colGui.a = 1f;
-		GUI.color = colGui;
-
-		//GUI.backgroundColor = Color.black;
-		if (currentMenu == "window1") 
-			Factory();
-		if (currentMenu == "window2") 
-			Tasks();
-		if (currentMenu == "window3") 
-			Developments ();
-		if (currentMenu == "window4")
-			Resources ();
-		if (currentMenu == "window5")
+		if (currentMenu == "window0")
+			Welcome ();
+		if (currentMenu == "window1a") 
 			Robots ();
+		if (currentMenu == "window1b")
+			RobotsB ();
+		if (currentMenu == "window2") 
+			Materials ();
+		if (currentMenu == "window3") 
+			Tabs ();
+		if (currentMenu == "window4")
+			Factory ();
+		if (currentMenu == "window5")
+			Developments ();
 		if (currentMenu == "window6")
-			Execute ();
+			Messages ();
 		if (currentMenu == "window7")
+			Tasks ();
+		if (currentMenu == "window8")
+			Energy ();
+		if (currentMenu == "window9")
+			Execute ();
+		if (currentMenu == "window10")
 			Play ();
+	}
+	void Welcome() {
+		if (GUI.Button (new Rect (startUpRect.x + 20, startUpRect.y + startUpRect.height - 35, 55, 25), "Next")) {
+			NavigateTo ("window1a");
+		}
+		if (GUI.Button (new Rect (startUpRect.x + 80, startUpRect.y + startUpRect.height - 35, 105, 25), "Skip Tutorial")) {
+			enabled = false;
+		}
+		GUI.Window (0, startUpRect, WindowFunction, "Welcome");
+	}
+	void Robots() {
+		if (GUI.Button (new Rect (robotRect.x + 40, robotRect.y + robotRect.height - 35, 55, 25), "Next")) {
+			NavigateTo("window1b");
+		}
+		if (GUI.Button (new Rect (robotRect.x + 100, robotRect.y + robotRect.height - 35, 65, 25), "Previous")) {
+			NavigateTo("window0");
+		}
+		GUI.Window (0, robotRect, WindowFunction, "Robots");
+	}
+	void RobotsB() {
+		if (GUI.Button (new Rect (robotBRect.x + 40, robotBRect.y + robotBRect.height - 35, 55, 25), "Next")) {
+			NavigateTo("window2");
+		}
+		if (GUI.Button (new Rect (robotBRect.x + 100, robotBRect.y + robotBRect.height - 35, 65, 25), "Previous")) {
+			NavigateTo("window1a");
+		}
+		GUI.Window (0, robotBRect, WindowFunction, "Robots");
 
 	}
-
-	void Factory() {
-		if (GUI.Button (new Rect (140, 250, 55, 25), "Next")) {
-			NavigateTo ("window2");
-		}
-		if (GUI.Button (new Rect (200, 250, 65, 25), "Previous")) {
-		}
-		GUI.Window (0, new Rect (100, 100, 200, 200), WindowFunction, "Factory");
-	}
-
-	void Tasks() {
-		if (GUI.Button (new Rect (440, 550, 55, 25), "Next")) {
+	void Materials() {
+		if (GUI.Button (new Rect (materialsRect.x + 40, materialsRect.y + materialsRect.height - 35, 55, 25), "Next")) {
 			NavigateTo("window3");
 		}
-		if (GUI.Button (new Rect (500, 550, 65, 25), "Previous")) {
-			NavigateTo("window1");
+		if (GUI.Button (new Rect (materialsRect.x + 100, materialsRect.y + materialsRect.height - 35, 65, 25), "Previous")) {
+			NavigateTo("window1b");
 		}
-		GUI.Window (0, new Rect (400, 400, 200, 200), WindowFunction, "Tasks");
+		GUI.Window (0, materialsRect, WindowFunction, "Materials");
+	}
+	void Tabs() {
+		if (GUI.Button (new Rect (tabsRect.x + tabsRect.width/2 - 57.5f, tabsRect.y + tabsRect.height - 35, 55, 25), "Next")) {
+			manager.onClickFactory();
+			NavigateTo("window4");
+		}
+		if (GUI.Button (new Rect (tabsRect.x + tabsRect.width/2 + 2.5f, tabsRect.y + tabsRect.height - 35, 65, 25), "Previous")) {
+			NavigateTo("window2");
+		}
+		GUI.Window (0, tabsRect, WindowFunction, "Tabs");
+
+	}
+	void Factory() {
+		if (GUI.Button (new Rect (factoryRect.x + 40, factoryRect.y + factoryRect.height - 35, 55, 25), "Next")) {
+			manager.onClickDevelopments();
+			NavigateTo("window5");
+		}
+		if (GUI.Button (new Rect (factoryRect.x + 100, factoryRect.y + factoryRect.height - 35, 65, 25), "Previous")) {
+			manager.onClickHome();
+			NavigateTo("window3");
+		}
+		GUI.Window (0, factoryRect, WindowFunction, "Factory");
 	}
 
 	void Developments() {
-		if (GUI.Button (new Rect (240, 350, 55, 25), "Next")) {
-			NavigateTo("window4");
-		}
-		if (GUI.Button (new Rect (300, 350, 65, 25), "Previous")) {
-			NavigateTo("window2");
-		}
-		GUI.Window (0, new Rect (200, 200, 200, 200), WindowFunction, "Developments");
-	}
-
-	void Resources() {
-
-		if (GUI.Button (new Rect (540, 350, 55, 25), "Next")) {
-			NavigateTo("window5");
-		}
-		if (GUI.Button (new Rect (600, 350, 65, 25), "Previous")) {
-			NavigateTo("window3");
-		}
-		GUI.Window (0, new Rect (500, 200, 200, 200), WindowFunction, "Resources");
-	}
-
-	void Robots() {
-		if (GUI.Button (new Rect (540, 350, 55, 25), "Next")) {
+		if (GUI.Button (new Rect (developmentsRect.x + 40, developmentsRect.y + developmentsRect.height - 35, 55, 25), "Next")) {
+			manager.onClickMessages();
 			NavigateTo("window6");
 		}
-		if (GUI.Button (new Rect (600, 350, 65, 25), "Previous")) {
+		if (GUI.Button (new Rect (developmentsRect.x + 100, developmentsRect.y + developmentsRect.height - 35, 65, 25), "Previous")) {
+			manager.onClickFactory();
 			NavigateTo("window4");
 		}
-		GUI.Window (0, new Rect (500, 200, 200, 200), WindowFunction, "Robots");
+		GUI.Window (0, developmentsRect, WindowFunction, "Developments");
 	}
-	void Execute() {
-		if (GUI.Button (new Rect (540, 350, 55, 25), "Next")) {
+	void Messages() {
+		if (GUI.Button (new Rect (messagesRect.x + 40, messagesRect.y + messagesRect.height - 35, 55, 25), "Next")) {
+			manager.onClickTasks();
 			NavigateTo("window7");
 		}
-		if (GUI.Button (new Rect (600, 350, 65, 25), "Previous")) {
+		if (GUI.Button (new Rect (messagesRect.x + 100, messagesRect.y + messagesRect.height - 35, 65, 25), "Previous")) {
+			manager.onClickDevelopments();
 			NavigateTo("window5");
 		}
-		GUI.Window (0, new Rect (500, 200, 200, 200), WindowFunction, "Execute");
+		GUI.Window (0, messagesRect, WindowFunction, "Messages");
+
+	}
+	void Tasks() {
+		if (GUI.Button (new Rect (tasksRect.x + 40, tasksRect.y + tasksRect.height - 35, 55, 25), "Next")) {
+			manager.onClickHome();
+			NavigateTo("window8");
+		}
+		if (GUI.Button (new Rect (tasksRect.x + 100, tasksRect.y + tasksRect.height - 35, 65, 25), "Previous")) {
+			manager.onClickMessages();
+			NavigateTo("window6");
+		}
+		GUI.Window (0, tasksRect, WindowFunction, "Tasks");
+	}
+	void Energy() {
+		if (GUI.Button (new Rect (energyRect.x + 40, energyRect.y + energyRect.height - 35, 55, 25), "Next")) {
+			NavigateTo("window9");
+		}
+		if (GUI.Button (new Rect (energyRect.x + 100, energyRect.y + energyRect.height - 35, 65, 25), "Previous")) {
+			manager.onClickTasks();
+			NavigateTo("window7");
+		}
+		GUI.Window (0, energyRect, WindowFunction, "Energy");
+
+	}
+	void Execute() {
+		if (GUI.Button (new Rect (executeRect.x + 40, executeRect.y + executeRect.height - 35, 55, 25), "Next")) {
+			NavigateTo("window10");
+		}
+		if (GUI.Button (new Rect (executeRect.x + 100, executeRect.y + executeRect.height - 35, 65, 25), "Previous")) {
+			NavigateTo("window8");
+		}
+		GUI.Window (0, executeRect, WindowFunction, "Execute");
 	}
 	void Play() {
-		if (GUI.Button (new Rect (540, 350, 75, 25), "Play Now")) {
+		if (GUI.Button (new Rect(playRect.x + playRect.width/2 - 40, playRect.y + playRect.height - 35, 80,25), "End Tutorial")) {
 			enabled = false;
 		}
-		GUI.Window (0, new Rect (500, 200, 200, 200), WindowFunction, "Time to Play!");
+		GUI.Window (0, playRect, WindowFunction, "Begin");
 	}
 
 	void WindowFunction(int windowID) {
-		if (currentMenu == "window1") {
-			GUI.Label (new Rect(10,20,180,180), factoryString);
+		if (currentMenu == "window0") {
+			GUI.Label (new Rect(10,20,startUpRect.width - 20,startUpRect.height - 20),startUpString);
+		}
+		if (currentMenu == "window1a") {
+			GUI.Label (new Rect(10,20,robotRect.width - 20,robotRect.height - 20), robotString);
+		}
+		if (currentMenu == "window1b") {
+			GUI.Label (new Rect (10, 20, robotBRect.width - 20, robotBRect.height - 20),robotBString);
 		}
 		if (currentMenu == "window2") {
-			GUI.Label (new Rect(10,20,180,180),tasksString);
+			GUI.Label (new Rect(10,20,materialsRect.width - 20,materialsRect.height - 20),materialsString);
 		}
 		if (currentMenu == "window3") {
-			GUI.Label (new Rect(10,20,180,180),developmentsString);
+			GUI.Label (new Rect(10,20,tabsRect.width - 20,tabsRect.height - 20),tabsString);
 		}
 		if (currentMenu == "window4") {
-			GUI.Label (new Rect(10,20,180,180),resourceString);
+			GUI.Label (new Rect(10,20,factoryRect.width - 20,factoryRect.height - 20),factoryString);
 		}
 		if (currentMenu == "window5") {
-			GUI.Label (new Rect (10, 20, 180, 180), robotString);
+			GUI.Label (new Rect (10, 20, developmentsRect.width - 20, developmentsRect.height - 20),developmentsString);
 		}
 		if (currentMenu == "window6") {
-			GUI.Label (new Rect (10,20,180,180),executeString);
+			GUI.Label (new Rect (10,20,messagesRect.width - 20,messagesRect.height - 20),messagesString);
 		}
 		if (currentMenu == "window7") {
-			GUI.Label (new Rect (10, 20, 180, 180), playString);
+			GUI.Label (new Rect (10, 20, tasksRect.width - 20, tasksRect.height - 20),tasksString);
+		}
+		if (currentMenu == "window8") {
+			GUI.Label (new Rect (10, 20, energyRect.width - 20, energyRect.height - 20),energyString);
+		}
+		if (currentMenu == "window9") {
+			GUI.Label (new Rect (10, 20, executeRect.width - 20, executeRect.height - 20),executeString);
+		}
+		if (currentMenu == "window10") {
+			GUI.Label (new Rect (10, 20, playRect.width - 20, playRect.height - 20),playString);
 		}
 	}
 	
 	void Update () {
+
 	}
 }
