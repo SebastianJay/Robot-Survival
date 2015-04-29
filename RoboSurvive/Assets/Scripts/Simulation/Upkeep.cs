56c3b1fa0090c651402744624a2690da46e381ff
Dylan Hellems <dylanhellems@gmail.com>
2015-04-29 01:04:06 -0400

    Added upkeep script

---
+++ RoboSurvive/Assets/Scripts/Simulation/Upkeep.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Upkeep : SimScript {
+
+	public float oilUsedMk1;
+	public float oilUsedMk2;
+	public float oilUsedMk3;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void RunSimulation(Transform preTurn, Transform afterTurn, Transform result) {
+		int totMk1 = afterTurn.GetComponent<RobotModel> ().GetMarkOne ();
+		int totMk2 = afterTurn.GetComponent<RobotModel> ().GetMarkTwo ();
+		int totMk3 = afterTurn.GetComponent<RobotModel> ().GetMarkThree ();
+
+		int oldOil = afterTurn.GetComponent<ResourceModel> ().GetOil ();
+		int newOil = oldOil - (oilUsedMk1 * totMk1 + oilUsedMk2 * totMk2 + oilUsedMk3 * totMk3);
+
+		result.GetComponent<ResourceModel> ().SetOil (newOil);
+	}
+}
